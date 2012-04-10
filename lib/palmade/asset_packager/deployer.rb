b939add502c851778eb7da358bd2974cb694fca7
Ramon Marco L. Navarro <ramonmaruko@gmail.com>
2012-04-10 15:52:50 +0800

    Allow app path to be specified via the config file
    This is so that we could specify an application that is
    from another manifest. e.g. Same assets server for both
    chronic apps and support-hub
    
    Change-Id: I7101ab211fea7d4996aa3eba33921a6e9d0ebd33

--- lib/palmade/asset_packager/deployer.rb
+++ lib/palmade/asset_packager/deployer.rb
@@ -1,3 +1,5 @@
+require 'pathname'
+
 module Palmade::AssetPackager
   class Deployer
     def initialize(options, apps = nil)
@@ -6,13 +8,12 @@ module Palmade::AssetPackager
         o.delete(:config_file)
       end
 
-      @apps    = get_apps(apps)
+      # Apps to bundle
+      @apps    = (apps.nil? or apps.empty?) ? get_apps : apps
     end
 
     def deploy
-      apps_root = get_apps_root
-
-      bundle_apps(apps_root, @apps)
+      bundle_apps(@apps)
     end
 
     protected
@@ -21,24 +22,61 @@ module Palmade::AssetPackager
       Palmade::AssetPackager.configuration.apps_root
     end
 
-    def get_apps(apps = nil)
-      (apps.nil? or apps.empty?) ?
-        Palmade::AssetPackager.configuration.apps :
-        apps
+    ##
+    # Gets apps that are in config. Not necessarily the apps to bundle.
+    #
+    def get_apps
+      Palmade::AssetPackager.configuration.apps
     end
 
-    def bundle_apps(apps_root, apps)
+    ##
+    # Returns the path to the application by concatenating
+    # apps_root and app_name.
+    #
+    # If the path is given in the configuration file and it is
+    # absolute, then it is returned; If not, it is taken to be
+    # relative to the apps_root
+    #
+    def get_abs_path(app_name)
+      app = get_apps.find do |app|
+        (app == app_name or app.include? app_name.to_sym) rescue false
+      end
+
+      app_path =
+        if app.respond_to?(:values)
+          app.values.first
+        else
+          app_name
+        end
+
+      app_path = Pathname.new(app_path)
+
+      app_path.absolute? ?
+        app_path :
+        File.expand_path(File.join(get_apps_root, app_path))
+    end
+
+    def get_app_name(app_name)
+      if app_name.respond_to?(:keys)
+        app_name.keys.first
+      else
+        app_name
+      end
+    end
+
+    def bundle_apps(apps)
       @logger.info "Bundling apps"
 
       apps.each do |app|
-        app_path = File.expand_path(File.join(apps_root, app))
+        app_path = get_abs_path(app)
+        app_name = get_app_name(app)
 
         unless File.exists?(app_path)
           @logger.error { "#{app_path} not found!" }
           next
         end
 
-        @logger.debug "Bundling #{app}"
+        @logger.debug "Bundling #{app_name}"
 
         if bundler = fork
           Process.wait2(bundler)
@@ -59,7 +97,6 @@ module Palmade::AssetPackager
           break
         end
       end
-
     end
 
   end
